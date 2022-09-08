69fbc4e517a300bf5f034d1b7b1af0a8eb2d89be
JM <georgemares14@yahoo.com>
2022-09-07 19:00:56 -0700

    Added get routes

--- Develop/server.js
+++ Develop/server.js
@@ -10,3 +10,21 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('public'));
+
+app.use(express.static('public'));
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/api/notes', (req, res)=> {
+    res.send(tests)
+});
+
+app.get('/notes', (req, res)=> {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.get('*', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
