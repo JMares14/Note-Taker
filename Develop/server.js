86418d49037ec5209fb68ddc315a9092316556ad
JM <georgemares14@yahoo.com>
2022-09-07 20:00:27 -0700

    Added post routes for notes

--- Develop/server.js
+++ Develop/server.js
@@ -28,3 +28,20 @@ app.get('*', (req,res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
+app.post('/api/notes', (req,res) =>{
+    const newNote = {title:req.body.title, text:req.body.text, id:uniqid() }
+    tests.push(newNote)
+    fs.writeFile('./db/db.json', JSON.stringify(tests), (err) => {
+        if (err) {console.log(err);}
+          
+        else {
+          console.log("Note added!");
+        }
+    })
+    res.send(tests)
+});
+
+
+app.listen (PORT, () =>{
+    console.log(`Listening on  ${PORT}`);
+});
